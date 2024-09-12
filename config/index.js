3e0701fcdf134ccdde976f8f073c9fb9a9b0be25
Shovon Saha <97457556+theshovonsaha@users.noreply.github.com>
2024-09-11 21:18:28 -0400

    Update index.js
    updated shown articles

--- config/index.js
+++ config/index.js
@@ -47,7 +47,7 @@ module.exports = {
     "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40theshovonsaha",
   // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
 
-  shownArticles: 2,
+  shownArticles: 5,
 
   //-- SOCIAL MEDIA SETTINGS -----
   // There are icons available for the following platforms:
